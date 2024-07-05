be72d4b4594a298f08398aae3bd1855896b014df
samergoda <judosamer555@gmail.com>
2024-07-05 21:12:52 +0300

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-
-import NavBarItem from "./NavbarItem.jsx"
+import { Suspense } from 'react';
+import NavBarItem from './NavbarItem.jsx';
 
 function NavBar() {
-    return (
-        <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
-            <NavBarItem title='Trending' param='fetchTrending' />
-            <NavBarItem title='Top Rated' param='fetchTopRated' />
-        </div>
-    )
+  return (
+    <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
+      <Suspense>
+        <NavBarItem title='Trending' param='fetchTrending' />
+      </Suspense>
+      <Suspense>
+        <NavBarItem title='Top Rated' param='fetchTopRated' />
+      </Suspense>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
