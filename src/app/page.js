2512ae7c08351f4ca5306784ed4b0a29e93adc89
samergoda <judosamer555@gmail.com>
2024-07-05 20:29:58 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import Results from "@/Components/Results";
+import Spinner from "@/Components/Spinner";
+
+const API_KEY = process.env.API_KEY;
+async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+
+  const res =  await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,{
+      next:{revalidate:60}
+    });
+
+    const data = await res.json()
+    if(!res.ok) throw new Error('failed to fetch data')
+    const result = data.results
+  // console.log(result)
+  return <div>
+    
+    {result?<Results result={result}/>:<Spinner />}
+  </div>;
+}
+
+export default Home;
